0aa210b35700d2c43523a0d68c02e6ce0b8048fd
Sk Shahnawaz <75124777+shahnawaz19@users.noreply.github.com>
2021-07-31 14:06:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ block_image_height=30;
 player_x=10
 player_y=10
 var player_object="";
+var block_image_object=""; 
 function player_update()
 {
 fabric.Image.fromURL("player.png",function(Img){
@@ -21,11 +22,11 @@ canvas.add(player_object);
 function new_image(get_image)
 {fabric.Image.fromURL(get_image,function(Img){
 block_image_object=Img;
-block_image_object.scaleToWidth=(block_image_width);
-block_image_object.scaleToHeight=(block_image_height);
+block_image_object.scaleToWidth(block_image_width);
+block_image_object.scaleToHeight(block_image_height);
 block_image_object.set(
-{top_player_y,
-left_player_x
+{top:player_y,
+left:player_x
 }    
 );
 canvas.add(block_image_object);
@@ -33,7 +34,7 @@ canvas.add(block_image_object);
 }
 window.addEventListener("keydown",my_keydown);
 function my_keydown(e)
-{keyPressed=e.keycode;
+{keyPressed=e.keyCode;
 console.log(keyPressed);
 if (e.shiftKey==true && keyPressed=='80')
 {console.log("p and shift pressed together");
@@ -98,7 +99,7 @@ console.log("y");
 }
 if(keyPressed=='85')
 {new_image('unique.png');
-console.log("u");
+console.log("u")
 }
 if(keyPressed=='67')
 {new_image('cloud.jpg');
@@ -106,4 +107,39 @@ console.log("c");
 }
 
 }
-
+function up()
+{if(player_y>=0)
+{player_y=player_y-block_image_height;
+console.log("block_image_height="+ block_image_height);
+console.log("when up arrow is pressed , x = " + player_x + "- y = " + player_y);
+canvas.remove(player_object);
+player_update();
+}
+}
+function down()
+{if(player_y<=500)
+{player_y=player_y+block_image_height;
+console.log("block_image_height="+ block_image_height);
+console.log("when down arrow is pressed , x = " + player_x + "# y = " + player_y);
+canvas.remove(player_object);
+player_update();
+}
+}
+function left()
+{if(player_x>=0)
+{player_x=player_x-block_image_width;
+console.log("block_image_width="+ block_image_width);
+console.log("when left arrow is pressed , x = " + player_x + "- y = " + player_y);
+canvas.remove(player_object);
+player_update();
+}
+}
+function right()
+{if(player_x<=700)
+{player_x=player_x+block_image_width;
+console.log("block_image_width="+ block_image_width);
+console.log("when right arrow is pressed , x = " + player_x + "- y = " + player_y);
+canvas.remove(player_object);
+player_update();
+}
+}
